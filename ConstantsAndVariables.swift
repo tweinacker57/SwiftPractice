5d9bf658424d86f9b328dbd36cce9a1491bc0fdf
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-20 13:42:37 -0400

    Print morning temperature

--- ConstantsAndVariables.swift
+++ ConstantsAndVariables.swift
@@ -4,3 +4,7 @@ let dailyTemperature = 75
 print("Today is \(day). Rise and Shine")
 print("The temperature on \(day) is \(dailyTemperature)")
 
+var temperature = 70
+
+print("The temperature on \(day) morning is \(temperature)")
+
