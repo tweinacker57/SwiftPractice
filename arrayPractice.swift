0d94ca8f82c4956967f30cf7e3e7f0ff4a84d37a
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-21 16:02:34 -0400

    Add first level score and bonus score. Print new value

--- arrayPractice.swift
+++ arrayPractice.swift
@@ -6,3 +6,6 @@ if levelScores.count == 0{
 let firstLevelScore = 10
 levelScores.append(firstLevelScore)
 print("Your first level score is \(levelScores[0])")
+let levelBonusScore = 40
+levelScores[0] = firstLevelScore + levelBonusScore
+print("Your updated first level score is \(levelScores[0])")
