4cf0fb7e7fc3dd235766e9e4cc60d5490a382b81
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-21 15:52:10 -0400

    Refactor levelScores to variable instaad of constant. Add first level score and append to level scores

--- arrayPractice.swift
+++ arrayPractice.swift
@@ -1,6 +1,8 @@
-let levelScores: [Int] = []
+var levelScores: [Int] = []
 
 if levelScores.count == 0{
     print("Welcome to the game")
 }
-
+let firstLevelScore = 10
+levelScores.append(firstLevelScore)
+print("Your first level score is \(levelScores[0])")
