819a3aef391aadbe357a11dbc0b857f9018d5311
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-21 10:40:44 -0400

    Add if else statement regarding morning and evening temperature

--- ConditionalStatements.swift
+++ ConditionalStatements.swift
@@ -3,3 +3,13 @@ let freeApp = true
 if freeApp == true {
     print("You are using the free version of the app. Buy the full version to get full access to features")
 }
+
+let morningTemperature = 70
+let eveningTemperature = 80
+
+if morningTemperature < eveningTemperature {
+    print(morningTemperature)
+}else {
+    print(eveningTemperature)
+}
+
