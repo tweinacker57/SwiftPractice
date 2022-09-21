7859aa3ace07abd0902ea7920bd10906a7d10d39
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-21 16:14:28 -0400

    Add freeLevelScores to levelScores. Create constant of number of free levels. Create if statement to show free levels

--- arrayPractice.swift
+++ arrayPractice.swift
@@ -9,3 +9,13 @@ print("Your first level score is \(levelScores[0])")
 let levelBonusScore = 40
 levelScores[0] = firstLevelScore + levelBonusScore
 print("Your updated first level score is \(levelScores[0])")
+let freeLevelScores = [20, 30]
+levelScores += freeLevelScores
+let freeLevels = 3
+if levelScores.count == freeLevels{
+    print("You have to buy the game in order to play its full version.")
+    levelScores = []
+    print("Game restarted!")
+}
+
+
