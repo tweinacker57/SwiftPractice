3fdfdb6b2bf2644d17edfe600cf5ae596340695b
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-21 13:27:44 -0400

    Add for in loop to iterate through levels

--- loopsPractice.swift
+++ loopsPractice.swift
@@ -2,3 +2,9 @@ let levels = 10
 let freeLevels = 4
 let bonusLevels = 3
 
+for level in 1...levels {
+    if level == bonusLevels {
+        print("Skip bonus level \(bonusLevels)")
+        continue
+    }
+}
