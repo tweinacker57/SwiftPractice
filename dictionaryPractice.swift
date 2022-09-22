74d42c75471b4050a5781ac2c5d840de6976fc13
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-22 15:38:47 -0400

    Add days and temperatures. Change the temperature for Monday. Print the temperature for Monday

--- dictionaryPractice.swift
+++ dictionaryPractice.swift
@@ -4,5 +4,6 @@ var weeklyTemperature: [String : Int] = [:]
 
 //Add days and temperature to the dictionary
 weeklyTemperature = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95, "Sunday": 100]
+weeklyTemperature["Monday"]! += 12
 
-print(weeklyTemperature)
+print("The temperature on Monday is \(weeklyTemperature["Monday"]!)")
