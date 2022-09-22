d49138279985b56c192aa1491c85c7692e7347a3
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-22 15:29:23 -0400

    Initial commit. Start weather app with dictionary of days and temperatures

---
+++ dictionaryPractice.swift
@@ -0,0 +1,6 @@
+//Create a "weather app". Start by creating an empty dictionary and assigning the types
+var weeklyTemperature = [:]
+weeklyTemperature = [String : Int]
+
+//Add days and temperature to the dictionary
+weeklyTemperature = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95, "Sunday": 100]
