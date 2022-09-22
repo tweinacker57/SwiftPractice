05566bcbca1aff87fff7a91e2906c822efa9c213
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-22 14:20:36 -0400

    Add comments for exercise

--- tuplesPractice.swift
+++ tuplesPractice.swift
@@ -1,4 +1,9 @@
+//Create credentials with an empty password and a negative number passcode
+
 let credentials = (password: "", passcode: -1111)
+
+//Run an if else statement to check validity of stored credentials
+//If password and passcode are incorrect, an error message will appear
 
 if credentials.0 == "" || credentials.1 < 0 {
     print("Invalid credentials!")
