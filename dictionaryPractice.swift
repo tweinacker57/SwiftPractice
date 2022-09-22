4aa66c340adfe25487384d951b4120d7ca86814c
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-22 15:31:11 -0400

    Refactor initial line of code because there was an error

--- dictionaryPractice.swift
+++ dictionaryPractice.swift
@@ -1,6 +1,8 @@
 //Create a "weather app". Start by creating an empty dictionary and assigning the types
-var weeklyTemperature = [:]
-weeklyTemperature = [String : Int]
+var weeklyTemperature: [String : Int] = [:]
+
 
 //Add days and temperature to the dictionary
 weeklyTemperature = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95, "Sunday": 100]
+
+print(weeklyTemperature)
