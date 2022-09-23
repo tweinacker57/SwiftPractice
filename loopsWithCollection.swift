91c950dbd94cb3f4aa5267c58ad9333b527ce2c0
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-23 09:16:35 -0400

    Add for in loop to loop through scores and add them all up

--- loopsWithCollection.swift
+++ loopsWithCollection.swift
@@ -3,3 +3,10 @@ let levelScores = [10, 20, 30, 40, 50, 60, 70]
 for(level, score) in levelScores.enumerated(){
     print("The score of level \(level + 1) is \(score)")
 }
+
+var gameScore = 0
+
+for score in levelScores{
+    gameScore += score
+    print("The final game score is \(gameScore)")
+}
