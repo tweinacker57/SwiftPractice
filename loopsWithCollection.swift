8d3246c85985a094fbb1b0f107d7ef8b522a4d1c
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-23 09:44:13 -0400

    Add new arrays and loop through to display days in proper order

--- loopsWithCollection.swift
+++ loopsWithCollection.swift
@@ -17,3 +17,9 @@ var weatherTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursd
 for(day, temp) in weatherTemperatures{
     print("The weather on \(day) is \(temp)")
 }
+//Create new arrays to put days in proper order
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+let temperatures = [70, 75, 80, 85, 90, 95, 100]
+for index in 0...6{
+    print("The temperature on \(days[index]) is \(temperatures[index])")
+}
