a143874c4a3b7a01768557772120f5e1c393b8a8
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-23 14:38:28 -0400

    Initial commit. Add constants for the game. Randomize number

---
+++ magicEightBall.swift
@@ -0,0 +1,5 @@
+//Game where a player asks a question and a magic 8-ball answers based off of a random number
+let playerName = "Tyler"
+let playerQuestion = "Will I be rich?"
+let randomNumber = Int.random(in 1...9)
+let eightBall = String
---
+++ moreFunctionsPractice.swift
@@ -0,0 +1,7 @@
+var goldBars = 0
+func incrementInventory(inventory: Int, amount: Int) -> Int {
+    amount = 100
+    return inventory + amount
+}
+
+
