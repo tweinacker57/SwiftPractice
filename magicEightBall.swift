25d7412c31a3df4be73efb3e88d60cc046b0563e
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-23 14:44:26 -0400

    Refactor random number constant. Refactor eightBall constant. Add switch with eight ball answers

--- magicEightBall.swift
+++ magicEightBall.swift
@@ -1,5 +1,29 @@
 //Game where a player asks a question and a magic 8-ball answers based off of a random number
 let playerName = "Tyler"
 let playerQuestion = "Will I be rich?"
-let randomNumber = Int.random(in 1...9)
-let eightBall = String
+let randomNumber = Int.random(in: 1...9)
+let eightBall: String
+
+switch randomNumber{
+case 1:
+    eightBall = "Yes, definitely"
+case 2:
+    eightBall = "It is decidedly so"
+case 3:
+    eightBall = "Without a doubt"
+case 4:
+    eightBall = "Reply is hazy, try again"
+case 5:
+    eightBall = "Ask again later"
+case 6:
+    eightBall = "Better not tell you now"
+case 7:
+    eightBall = "My sources say no"
+case 8:
+    eightBall = "Very doubtful"
+case 9:
+    eightBall = "Outlook is not so good"
+default:
+    eightBall = "Error"
+}
+
