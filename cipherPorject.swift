926f4eb5921bd4022279fa4203b21047ee406a9f
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-27 14:05:29 -0400

    initial commit for cipher project. Start by adding array of alphabet

---
+++ cipherPorject.swift
@@ -0,0 +1,3 @@
+//Create a cipher using Caeaser's ciphering method
+let alphabet: [Character] = ["a", "b", "c", "d", "e", "f". "g" "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
