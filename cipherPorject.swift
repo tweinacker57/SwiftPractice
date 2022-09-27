08237137581f6c8032edba0d0ce59218426c1d54
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-27 14:08:26 -0400

    Refactor alphabet as there were a few mistakes. Add message and convert secretMessage to an array

--- cipherPorject.swift
+++ cipherPorject.swift
@@ -1,3 +1,8 @@
 //Create a cipher using Caeaser's ciphering method
-let alphabet: [Character] = ["a", "b", "c", "d", "e", "f". "g" "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+let alphabet: [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//Add message
+var message = "hello"
+//Add secret message variable and convert message into an array
+var secretMessage = Array(message)
 
+print(secretMessage)
