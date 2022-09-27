2bec46685dd0a24bd09e9975ec6cae879eb91795
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-27 10:43:39 -0400

    Add function to call raw value of enums. Print opinions

--- enumsPractice.swift
+++ enumsPractice.swift
@@ -4,7 +4,12 @@ enum Bands: String {
     case BMTH = "One of my favorite bands"
     case FFAA = "These guys are awesome"
     case Beatles = "Worst band in the world"
-    case Dayseeker= "Slowly becoming my favorite band"
+    case Dayseeker = "Slowly becoming my favorite band"
 }
 
+func getTylersOpinion (on Bands: Bands){
+    print(Bands.rawValue)
+}
 
+getTylersOpinion(on: .Dayseeker)
+getTylersOpinion(on: .Beatles)
