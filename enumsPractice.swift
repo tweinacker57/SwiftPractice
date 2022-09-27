80890c87fa87ca3b5e2d16e8d741ff660ca68e2d
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-27 10:39:04 -0400

    Initial commit for this file. Add enums

---
+++ enumsPractice.swift
@@ -0,0 +1,10 @@
+//Practice with enums
+
+enum Bands: String {
+    case BMTH = "One of my favorite bands"
+    case FFAA = "These guys are awesome"
+    case Beatles = "Worst band in the world"
+    case Dayseeker= "Slowly becoming my favorite band"
+}
+
+
--- modularize.swift
+++ modularize.swift
@@ -1,13 +1,13 @@
 //Program to keep track of gold bars spent
 var goldBars = 100
-//Add a function to accept inventory
-func spendTenGoldBars(from inventory: inout Int, completion: (Int) -> Void) {
+
+func spendTenGoldBars(from inventory: inout Int, completion:(Int) -> Void){
     inventory -= 10
     completion(inventory)
 }
 print("You had \(goldBars) gold bars.")
-spendTenGoldBars(from: &goldBars) { goldBars in
-    print("You spent ten gold bars")
-    print("You now have \(goldBars) remaining gold bars")
-}
 
+spendTenGoldBars(from: &goldBars){ goldBars in
+    print("You spent ten gold bars.")
+    print("You have \(goldBars) remaining gold bars")
+}
