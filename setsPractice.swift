d8e172732dd15c15b3e9a75068c8b92bee6ae4f1
Tyler Weinacker <tweinacker57@gmail.com>
2022-09-28 09:43:28 -0400

    Initial commit. Use isEmpty and count methods

---
+++ setsPractice.swift
@@ -0,0 +1,9 @@
+// Utilize the isEmpty and count method
+
+var sockDrawer = ["Red stripes", "Blue dress", "Plain white"]
+
+if sockDrawer.isEmpty{
+    print("You need to buy socks!")
+}else {
+    print("We have \(sockDrawer.count) pairs of socks in the drawer")
+}
